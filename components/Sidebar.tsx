dd7ba287c528def91fbacee71339b16336fe20ce
Richard <richard.yang.tong@gmail.com>
2021-04-13 09:44:55 -0400

    Implemented the font.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,8 +10,8 @@ const Sidebar = () => {
                 alt='user avatar'
                 className='w-32 h-32 mx-auto rounded-full'
             />
-            <h3 className='my-4 tex-3x1 font-medium tracking-wider'>
-                <span>Richard</span>Yang
+            <h3 className='my-4 text-3x1 font-medium tracking-wider font-kaushan'>
+                <span>Richard</span> Yang
             </h3>
             <p className='px-2 py-1 my-3 bg-green-200 rounded-full'>
                 Web Developer
