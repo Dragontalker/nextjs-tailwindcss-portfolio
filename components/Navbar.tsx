babcd985fe11956528b1bf60b505f9b9f537689e
Richard <richard.yang.tong@gmail.com>
2021-04-13 13:49:47 -0400

    Finished Navbar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,45 +1,66 @@
-import { useState, useEffect } from 'react'
-import Link from 'next/link'
+import { useState, useEffect, FunctionComponent } from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const NavItem: FunctionComponent<{
+   active: string
+   setActive: Function
+   name: string
+   route: string
+}> = ({ active, setActive, name, route }) => {
+   return active !== name ? (
+      <Link href={route}>
+         <a>
+            <span
+               className='mx-2 cursor-pointer hover:border-b-4 hover:text-green'
+               onClick={() => setActive(name)}>
+               {name}
+            </span>
+         </a>
+      </Link>
+   ) : null
+}
 
 const Navbar = () => {
+   const { pathname } = useRouter()
 
-    const [activeItem, setActiveItem] = useState<string>('')
+   const [active, setActive] = useState('')
 
-    const { pathname } = useRouter()
-    
-    useEffect(() => {
-        if (pathname === '/') setActiveItem('About')
-    }, [])
+   //later
+   useEffect(() => {
+      if (pathname === '/') setActive('About')
+      else if (pathname === '/projects') setActive('Projects')
+      else if (pathname === '/resume') setActive('Resume')
+   }, [])
 
-    return (
-        <div>
-            <span className='font-bold text-green'>{activeItem}</span>
-            <div className='flex space-x-3 text-red-400 font-lg'>
-                {activeItem !== 'About' && (
-                    <Link href="/">
-                        <a>
-                            <span onClick={() => setActiveItem('About')}>About</span>
-                        </a>
-                    </Link>
-                )}
-                {activeItem !== 'Projects' && (
-                    <Link href="/projects">
-                        <a>
-                            <span onClick={() => setActiveItem('Projects')}>Projects</span>
-                        </a>
-                    </Link>
-                )}
-                {activeItem !== 'Resume' && (
-                    <Link href="/resume">
-                        <a>
-                            <span onClick={() => setActiveItem('Resume')}>Resume</span>
-                        </a>
-                    </Link>
-                )}
-            </div>
-        </div>
-    )
+   return (
+      <div className='flex items-center justify-between px-5 py-3 my-3'>
+         <span className='text-xl font-bold border-b-4 md:text-2xl border-green'>
+            {active}
+         </span>
+
+         <div className='text-base font-normal md:text-xl'>
+            <NavItem
+               active={active}
+               setActive={setActive}
+               name='About'
+               route='/'
+            />
+            <NavItem
+               active={active}
+               setActive={setActive}
+               name='Resume'
+               route='/resume'
+            />
+            <NavItem
+               active={active}
+               setActive={setActive}
+               name='Projects'
+               route='/projects'
+            />
+         </div>
+      </div>
+   )
 }
 
 export default Navbar
