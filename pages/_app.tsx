e175a7fb4b8544219010f0b99916b0ef87b8a327
Richard <richard.yang.tong@gmail.com>
2021-04-12 20:18:35 -0400

    Set the grid layout for different components.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <div className='grid grid-cols-12 gap-6'>
-      <div>Sidebar</div>
-      <div>
+      <div className='col-span-3'>Sidebar</div>
+      <div className='col-span-9'>
         <Component {...pageProps} />
       </div>
     </div>
