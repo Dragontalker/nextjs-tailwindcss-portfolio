58a68c48a5a32476796f5b5582553cd514933c04
Richard <richard.yang.tong@gmail.com>
2021-04-12 20:23:00 -0400

    Added responsiveness.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <div className='grid grid-cols-12 gap-6 my-14 px-48'>
-      <div className='col-span-3 bg-white rounded-2xl'>Sidebar</div>
-      <div className='col-span-9 bg-white rounded-2xl'>
+      <div className='lg:col-span-3 bg-white rounded-2xl col-span-12'>Sidebar</div>
+      <div className='lg:col-span-9 bg-white rounded-2xl col-span-12'>
         <Component {...pageProps} />
       </div>
     </div>
