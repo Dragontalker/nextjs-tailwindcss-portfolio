05985919d9d884d17155cc2f38e3281766d68808
Richard <richard.yang.tong@gmail.com>
2021-04-12 20:22:04 -0400

    Added more styles.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='grid grid-cols-12 gap-6'>
-      <div className='col-span-3 bg-white'>Sidebar</div>
-      <div className='col-span-9 bg-white'>
+    <div className='grid grid-cols-12 gap-6 my-14 px-48'>
+      <div className='col-span-3 bg-white rounded-2xl'>Sidebar</div>
+      <div className='col-span-9 bg-white rounded-2xl'>
         <Component {...pageProps} />
       </div>
     </div>
