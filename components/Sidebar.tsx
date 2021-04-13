2d828a1e4c2043b41ee83d733ee562cbd3150ed7
Richard <richard.yang.tong@gmail.com>
2021-04-13 09:49:35 -0400

    Applied custom color.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
                 <GiTie className='w-6 h-6'/>Download Resume
             </a>
             {/* social icon */}
-            <div className='flex justify-around my-5 text-green-500 md:w-full w-9/12 mx-auto'>
+            <div className='flex justify-around my-5 text-green md:w-full w-9/12 mx-auto'>
                 <a>
                     <AiFillYoutube className='w-8 h-8 cursor-pointer'/>
                 </a>
@@ -46,8 +46,8 @@ const Sidebar = () => {
                 <p className='my-2'>richard.yang.tong@gmail.com</p>
                 <p className='my-2'>+1(647)533-7683</p>
             </div>
-            <button className='bg-gradient-to-r from-green-400 to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2'>Email Me</button>
-            <button className='bg-gradient-to-r from-green-400 to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2'>Toggle Theme</button>
+            <button className='bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2'>Email Me</button>
+            <button className='bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2'>Toggle Theme</button>
         </div>
     )
 }
