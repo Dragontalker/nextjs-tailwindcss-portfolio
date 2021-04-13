1708533d6d9b1978a834e08e908d9f2a5526d772
Richard <richard.yang.tong@gmail.com>
2021-04-12 20:29:39 -0400

    Added a user avatar.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,10 @@
 const Sidebar = () => {
     return (
         <div>
-            
+            <img 
+                src='https://dragontalker.github.io/FullStack-DeveloperPortfolio/Assets/Image/profile_picture.jpg'
+                alt='user avatar'
+            />
         </div>
     )
 }
