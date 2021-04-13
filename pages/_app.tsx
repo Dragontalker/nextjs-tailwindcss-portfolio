0d508cf80b37c717c20e4073737376ca128caf91
Richard <richard.yang.tong@gmail.com>
2021-04-12 20:19:23 -0400

    Set the background of different grid to white to make them visible.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <div className='grid grid-cols-12 gap-6'>
-      <div className='col-span-3'>Sidebar</div>
-      <div className='col-span-9'>
+      <div className='col-span-3 bg-white'>Sidebar</div>
+      <div className='col-span-9 bg-white'>
         <Component {...pageProps} />
       </div>
     </div>
